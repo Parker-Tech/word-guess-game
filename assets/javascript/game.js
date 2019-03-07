e0746d47443df425bcb9e323561a846ded2d693c
JP <parker.tech9@gmail.com>
2019-03-07 02:43:39 -0500

    have Letters Guessed and Number of guesses done. Need to figure out win and loss conditions since i just cant reset the page --> i need to keep track of wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,28 @@
-var wordList = ["astronomy", "astrophysics", "atom",
-
-"beaker", "biochemistry", "biology", "botany", "burette", // "Bunsen burner",
-
-"cell", "chemical", "chemistry", "climate", "climatologist", "control", "cuvette",
-
-"data", "datum",
-
-"electricity", "electrochemist", "element", "energy", "entomology", "evolution", "experiment",
-
-"fact", "flask", "fossil", "funnel",
-
-"genetics", "geology", "geophysics", "glassware", "graduated", "cylinder", "gravity", 
-
-"herpetology", "hypothesis", 
-
-"ichthyology", "immunology", 
-
-"lab", "laboratory", "lawslepidoptery", 
-
-"magnetism", "mass", "matter", "measure", "meteorologist", "meteorology", "microbiologist", "microbiology", "microscopemineral", "mineralogy", "molecule", "motion",
-
-"observatory", "observe", "organism", "ornithology",
-
-"article", "phase", "physics", "pipette", // "physical science", "Petri dish", 
-
-//"quantum mechanics", 
-
-"radiology", "research", "retort", 
-
-"scale", "science", "scientist", "seismology", 
-
-"telescope", "temperature", "theory", "thermometer", "tissue", // "test tube",
-
-"variable", "virologist", "volcano", "volume", //"volumetric flask", 
-
- "weather", "weigh", //"watch glass",
-
-"zoology"]
-
+var wordList = ["astronomy", "astrophysics", "atom", "beaker", "biochemistry", "biology", "botany", "burette","cell", "chemical", "chemistry", "climate", "climatologist", "control", "cuvette","data", "datum","electricity", "electrochemist", "element", "energy", "entomology", "evolution", "experiment","fact", "flask", "fossil", "funnel","genetics", "geology", "geophysics", "glassware", "graduated", "cylinder", "gravity", "herpetology", "hypothesis", "ichthyology", "immunology", "lab", "laboratory", "magnetism", "mass", "matter", "measure", "meteorologist", "meteorology", "microbiologist", "microbiology", "mineralogy", "molecule", "motion","observatory", "observe", "organism", "ornithology","article", "phase", "physics", "pipette","radiology", "research", "retort", "scale", "science", "scientist", "seismology", "telescope", "temperature", "theory", "thermometer", "tissue","variable", "virologist", "volcano", "volume","weather", "weigh","zoology"]
 
 var word = wordList[Math.floor(Math.random() * wordList.length)];
 var initialUnderscore = document.getElementById("placeholder");
 var changeUnderscore;
 var testingVar = document.getElementById("testing");
+var guessesLeftStr;
 var allGuessArr = [];
 var wrongGuesses = [];
 var correctGuesses = [];
 var underScoreArr = [];
-var guessesLeft = 13;
+var userInput = document.onkeydown;
+var winCheckStr;
+var wrongGuessesTaken = 0;
+var totalAllowedGuesses = 13;
 var theChoice;
-var choiceIndex;
 var underScoreNew;
 var indexArray = []; 
 var indicies = [];
 var regex0 = /^[a-z]*$/
 
+
+for(i = 0; i < word.length; i++) { 
+  initialUnderscore.insertAdjacentHTML("beforeend", "_");
+}
 
 function replaceCharInUnderscore(chosenWord, underScores, char) {
   for(i = 0; i < chosenWord.length; i++){
@@ -63,14 +31,7 @@ function replaceCharInUnderscore(chosenWord, underScores, char) {
       document.getElementById("placeholder").innerHTML = underScores;
     }
   }
-}
-
-function findMultipleIndecies(str) { //using this as a counter for multiple instances of a char in word
-  for(i = 0; i < str.length; i++){
-    if(str[i] == theChoice) {
-      indexArray.push(i);
-    }
-  }
+  winCheckStr = underScores;
 }
 
 function letterCheck(char) {
@@ -81,62 +42,125 @@ function letterCheck(char) {
   }
 }
 
-for(i = 0; i < word.length; i++) { 
-  initialUnderscore.insertAdjacentHTML("beforeend", "_");
+function winCheck(str) {
+  if(str == word){
+    document.getElementById("jumbotron-win-screen").innerHTML("<h1>You Win!</h1>").setAttribute("id", "large-win-font");
+    
+    // var reloadConfirm = confirm("You Win!\nPlay Again?")
+    // if(reloadConfirm){
+    //   location.reload();
+    // }else{
+    //   //something funny?
+    // }
+  }
 }
 
+// function charFilter(event) {
+
+//   theChoice = event.key.toLowerCase();
+  
+//   if(letterCheck(theChoice)){
+
+//     allGuessArr.push(theChoice);  // use allGuessArr to check if the key has been hit already
+
+//     if(word.includes(theChoice)){ //add && theChoice.isNotIn(correctGuessArray)   
+
+//       changeUnderscore = document.getElementById("placeholder").innerHTML;
+//       correctGuesses.push(theChoice);
+//       replaceCharInUnderscore(word, changeUnderscore, theChoice);
+
+//     }else{
+
+//       theChoice = theChoice.toUpperCase();
+
+//       if(wrongGuessesTaken == 0){
+//         document.getElementById("letters-guessed").insertAdjacentText("beforeend", theChoice);
+//       }else{
+//         document.getElementById("letters-guessed").insertAdjacentText("beforeend",  " | " + theChoice);
+//       }
+
+//       if(wrongGuessesTaken == 13){
+//         var reloadConfirm = confirm("You Lose\nTry Again?")   //play Bruh sound effect #2
+//         if(reloadConfirm){
+//           location.reload();
+//         }else{
+//           //something funny?
+//         }
+//       }
+      
+//       wrongGuessesTaken++;
+
+//       document.getElementById("number-guesses-left").innerText = "Number of Guesses Left: " + (totalAllowedGuesses - wrongGuessesTaken);
+
+
+//     }
+
+//   }else {
+
+//     alert("No numbers, spaces or special characters are in any of these words.");
+
+//   }
+
+// }
+
+// testingVar.insertAdjacentHTML("beforeend", word); //just for testing the word
+
+// document.getElementById("number-guesses-left").insertAdjacentHTML("beforeend", totalAllowedGuesses)
+
+// document.onkeydown = charFilter();
+// winCheck(winCheckStr);
+
 testingVar.insertAdjacentHTML("beforeend", word); //just for testing the word
+
+document.getElementById("number-guesses-left").insertAdjacentHTML("beforeend", totalAllowedGuesses)
 
 document.onkeydown = function(event) {
 
   theChoice = event.key.toLowerCase();
+  
   if(letterCheck(theChoice)){
 
     allGuessArr.push(theChoice);  // use allGuessArr to check if the key has been hit already
 
-    console.log(allGuessArr.length);
+    if(word.includes(theChoice)){ //add && theChoice.isNotIn(correctGuessArray)   
 
-    if(word.includes(theChoice)){ //add && theChoice.isNotIn(correctGuessArray)
-      
-      findMultipleIndecies(word);
-      console.log(allGuessArr);
-      choiceIndex = word.indexOf(theChoice);
-      choiceIndex2 = word.indexOf(theChoice, choiceIndex + 1);
-      // console.log(choiceIndex2);
       changeUnderscore = document.getElementById("placeholder").innerHTML;
       correctGuesses.push(theChoice);
       replaceCharInUnderscore(word, changeUnderscore, theChoice);
 
-      // if(indexArray.length == 1 || indexArray.length == 0){
-      //   // document.getElementById("placeholder").innerHTML = changeUnderscore.replaceCharInUnderscore(choiceIndex, theChoice);//you have to do this bc strings are immutable in javascript
-      //   replaceCharInUnderscore(word, changeUnderscore, theChoice);
-      // }else if(indexArray.length == 2){           
-      //   // document.getElementById("placeholder").innerHTML = changeUnderscore.replaceTwo(choiceIndex, choiceIndex2, theChoice);
-      //   replaceCharInUnderscore(word, changeUnderscore, theChoice);
-      //   // console.log(changeUnderscore);
-      // }else if(indexArray.length == 3){         
-      //   // document.getElementById("placeholder").innerHTML = changeUnderscore.replaceThree(allGuessArr, theChoice);
-      //   replaceCharInUnderscore(word, changeUnderscore, theChoice);
-      // }else{
-      //   // document.getElementById("placeholder").innerHTML = "you done messed up A-Aron";
-
-      //   // console.log(allGuessArr);
-      // }
-      console.log(changeUnderscore);
-      console.log(choiceIndex); 
     }else{
-      alert("You failed!");
-      wrongGuesses.push(theChoice);
-      guessesLeft--;
+
+      theChoice = theChoice.toUpperCase();
+
+      if(wrongGuessesTaken == 0){
+        document.getElementById("letters-guessed").insertAdjacentText("beforeend", theChoice);
+      }else{
+        document.getElementById("letters-guessed").insertAdjacentText("beforeend",  " | " + theChoice);
+      }
+
+      if(wrongGuessesTaken == 13){
+        var reloadConfirm = confirm("You Lose\nTry Again?")   //play Bruh sound effect #2
+        if(reloadConfirm){
+          location.reload();
+        }else{
+          //something funny?
+        }
+      }
+      
+      wrongGuessesTaken++;
+
+      document.getElementById("number-guesses-left").innerText = "Number of Guesses Left: " + (totalAllowedGuesses - wrongGuessesTaken);
+
+
     }
-    indexArray = []; //resets array so it doesnt stack with all the other indecies     <<<<<<< This was required for selecteMultipleIndecies() !!!!!!!!
+
   }else {
+
     alert("No numbers, spaces or special characters are in any of these words.");
+
   }
 
-  if(word == changeUnderscore){
-    alert("wow you actually won");
-  }
-  console.log(word)
-  // console.log(replaceLastUnderScore)
+  console.log(changeUnderscore)
+  winCheck(winCheckStr);
+
 }
